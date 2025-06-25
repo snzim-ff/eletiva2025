d4094b233bf6b4f1a34d8ef7c1702539e5323ae6
snzim-ff <joaopaulosho2026@gmail.com>
2025-06-25 15:02:28 +0000

    primeira aula de programção

---
+++ main.py
@@ -0,0 +1,12 @@
+peso = float(input("Qual o seu peso?"));
+altura = float(input("Qual a sua altura?"));
+imc = (peso/(altura*altura));
+print(imc);
+print(f"Seu IMC é: {imc: .2f}");
+
+if (imc<18.5):
+    print "Magreza"
+    elif(18.5 <= imc <= 24.9):
+        print("Normal")
+        elif (imc>20):
+            print("teste")
