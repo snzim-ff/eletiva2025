f2c28bfff3c65f426c1c6b2899361310c0f05956
snzim-ff <joaopaulosho2026@gmail.com>
2025-07-02 15:03:37 +0000

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+#  CALCULADORA SOLICITADA NA SEGUNDA AULA DA ELETIVA DE
+# MATEMÁTICA, TECNOLOGIAS E COMUDIDADES. 2025
+
+
+def soma(numero1,numero2);
+numero1=float(input("Qual é o número correspodente a váriave, em sua determinada ordem, numero1?"));
+numero2=float(input("Qual é o número correspodente a váriave, em sua determinada ordem, numero2?"));
+print (numero1)
